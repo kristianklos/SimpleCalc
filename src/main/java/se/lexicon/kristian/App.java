7f81a668d9a997c6ed39e026121d3d6535521839
Kristian Klos <kristianklos@gmail.com>
2019-09-03 11:32:24 +0200

    Simple Calculator

--- src/main/java/se/lexicon/kristian/App.java
+++ src/main/java/se/lexicon/kristian/App.java
@@ -1,61 +1,67 @@
 package se.lexicon.kristian;
 
-import java.util.*;
 import java.util.Scanner;
 
-        //asking for two numbers
-abstract class App {
-    public static void main(String[] args)
-    {
+//asking for two numbers
+public class App {
+    public static void main(String[] args) {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        //checking input
+        Scanner inp = new Scanner(System.in);
         int num1;
         num1 = inp.nextInt();
         int num2 = inp.nextInt();
         int ans;
-        System.out.println("Enter your arithmetic: \n[1] Addition "+
-                                                  "\n[2] Substraction " +
-                                                  "\n[3] Multiplication " +
-                                                  "\n[4] Division");
+        //choose arithmetic
+        System.out.println("Enter your arithmetic: \n[1] Addition " +
+                "\n[2] Substraction " +
+                "\n[3] Multiplication " +
+                "\n[4] Division");
 
-        //pick which arithmetic you want to use
+        //checking which arithmetic you picked
         int choose;
         choose = inp.nextInt();
 
         //array for arithmetic with println
-        switch (choose){
+        switch (choose) {
             case 1:
-                System.out.println("Answer:" + add( num1,num2));
+                System.out.println("Answer:" + add(num1, num2));
                 break;
             case 2:
-                System.out.println("Answer:" + sub( num1,num2));
+                System.out.println("Answer:" + sub(num1, num2));
                 break;
             case 3:
-                System.out.println("Answer:" + mult( num1,num2));
+                System.out.println("Answer:" + mult(num1, num2));
                 break;
             case 4:
-                System.out.println("Answer:" + div( num1,num2));
+                System.out.println("Answer:" + div(num1, num2));
                 break;
             default:
-                System.out.println("Error");
+                System.out.println("Error!");
         }
     }
-        //doing the calculations
-    private static int add(int x, int y)
-    {
+
+    //doing the calculations
+    private static int add(int x, int y) {
         return x + y;
     }
-    private static int sub(int x, int y)
-    {
-        return x-y;
+
+    private static int sub(int x, int y) {
+        return x - y;
     }
-    private static int mult(int x, int y)
-    {
-        return x*y;
+
+    private static int mult(int x, int y) {
+        return x * y;
     }
-    private static int div(int x, int y)
-    {
-        return x/y;
+
+    private static int div(int x, int y) {
+        //fixing the /0 crash
+        if (y <= 0) {
+           return 0;
+        }
+        else {
+            return x / y;
+        }
     }
 }
 
